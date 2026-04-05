d89ffd30f4d92b0ca02f5a5226bd721d40ccab07
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-04-05 20:07:06 +0100

    refactor: import initState

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { startRepl } from "./repl.js";
+import { initState } from "./state.js";
 
-function main(): void {
-    startRepl();
+async function main(): Promise<void> {
+  const stateObj = await initState();
+  await startRepl(stateObj);
 }
 
 main();
