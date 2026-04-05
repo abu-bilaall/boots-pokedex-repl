803ac012b5c769e885d5fdc34bfd1bb1bd4e30dc
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-04-05 20:08:37 +0100

    refactor: add State support

--- src/repl.ts
+++ src/repl.ts
@@ -1,59 +1,42 @@
-import { createInterface } from "node:readline";
-import type { CLICommand } from "./command.js";
-import { commandExit } from "./command_exit.js";
-import { commandHelp } from "./command_help.js";
+import { State } from "./state.js";
 
-export function getCommands(): Record<string, CLICommand> {
-    return {
-        exit: {
-            name: "exit",
-            description: "Exits the pokedex",
-            callback: commandExit,
-        },
-        help: {
-            name: "help",
-            description: "Displays a help message",
-            callback: commandHelp,
-        },
-        // more commands go here
-    };
-}
+export async function startRepl(stateObj: State) {
+  const rl = stateObj.rl;
+  const commands = stateObj.commands;
 
-export function startRepl() {
-    const rl = createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: "Pokedex > "
-    });
-    
-    // display prompt
-    rl.prompt();
+  // display prompt
+  rl.prompt();
 
-    // listening on line
-    rl.on("line", (input) => {
-        if (!input) {
-            rl.prompt();
-        } else {
-            const inputArr = cleanInput(input);
-            const userCmd = inputArr[0];
-            const supportedCmds = getCommands();
-            if (supportedCmds[userCmd]) {
-                try {
-                    supportedCmds[userCmd].callback(supportedCmds);
-                } catch (error) {
-                    console.log(error);
-                }
-            } else {
-                console.log("Unknown command");
-            }
+  // listening on line
+  rl.on("line", async (input) => {
+    if (!input) {
+      rl.prompt();
+    } else {
+      const inputArr = cleanInput(input);
+      const userCmd = inputArr[0];
+      const location = inputArr[1] ? inputArr[1] : undefined;
+      if (commands[userCmd]) {
+        try {
+          if (location) {
+            await commands[userCmd].callback(stateObj, location);
+          } else {
+            await commands[userCmd].callback(stateObj);
+          }
+          console.log();
+        } catch (error) {
+          console.log((error as Error).message);
         }
+      } else {
+        console.log("Unknown command\n");
+      }
+    }
 
-        rl.prompt();
-    });
+    rl.prompt();
+  });
 }
 
 export function cleanInput(input: string): string[] {
-    const cleanInput = input.trim().toLowerCase();
-    const inputArr = cleanInput.split(" ");
-    return inputArr.filter((entry) => entry !== "");
+  const cleanInput = input.trim().toLowerCase();
+  const inputArr = cleanInput.split(" ");
+  return inputArr.filter((entry) => entry !== "");
 }
