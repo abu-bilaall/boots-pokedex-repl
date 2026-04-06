894994f54a717bb9d16a707580b1c202b5b9787a
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-04-06 08:53:32 +0100

    feat: add logic for inspect command

---
+++ src/command_inspect.ts
@@ -0,0 +1,25 @@
+import { State } from "./state.js";
+
+export async function inspect(state: State, pokemonName?: string) {
+  if (pokemonName === undefined) {
+    console.log("You must specify a pokemon name");
+    return;
+  }
+
+  for (const entry of state.pokedex) {
+    if (entry.name === pokemonName){
+      const statsOutput = entry.stats.map((stat) => `-${stat.stat.name}: ${stat.base_stat}`);
+      const typesOutput = entry.types.map((type) => `-${type.type.name}`);
+      console.log(`
+Name: ${entry.name}
+Height: ${entry.height}
+Weight: ${entry.weight}
+Stats:
+\t${statsOutput.join("\n\t")}
+Types:
+\t${typesOutput.join("\n\t")}`);
+    }
+  }
+
+  console.log("You have not caught that pokemon");
+}
