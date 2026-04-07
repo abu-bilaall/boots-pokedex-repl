914406b68286418b04796419d01829390256e387
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-04-07 14:35:57 +0100

    feat: add fetchPokemonInfo method

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { Cache } from "./pokecache.js";
 
 type ShallowLocations = {
@@ -24,6 +25,37 @@ interface Pokemon {
   name: string;
   url: string;
 }
+
+const EndpointResponseSchema = z.object({
+  base_experience: z.number(),
+  forms: z.array(z.object({ name: z.string(), url: z.string() })),
+  height: z.number(),
+  weight: z.number(),
+  stats: z.array(
+    z.object({
+      base_stat: z.number(),
+      effort: z.number(),
+      stat: z.object({ name: z.string(), url: z.string() }),
+    }),
+  ),
+  types: z.array(
+    z.object({
+      slot: z.number(),
+      type: z.object({ name: z.string(), url: z.string() }),
+    }),
+  ),
+});
+
+type EndpointResponse = z.infer<typeof EndpointResponseSchema>;
+
+type pokemonInfoType = {
+  base_exp: EndpointResponse["base_experience"];
+  name: EndpointResponse["forms"][number]["name"];
+  height: EndpointResponse["height"];
+  weight: EndpointResponse["weight"];
+  stats: EndpointResponse["stats"];
+  types: EndpointResponse["types"];
+};
 
 class PokeAPI {
   private static readonly baseURL = "https://pokeapi.co/api/v2";
@@ -62,6 +94,35 @@ class PokeAPI {
     PokeAPI.#pokecache.add(fullUrl, resObj);
     return resObj;
   }
+
+  async fetchPokemonInfo(pokemonName: string) {
+    const urlEndpoint = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+    const res = await fetch(urlEndpoint);
+
+    if (!res.ok) {
+      console.log(`${pokemonName} not found!`);
+    }
+
+    const json = await res.json();
+    const resObj: EndpointResponse = EndpointResponseSchema.parse(json);
+
+    const pokemonInfo: pokemonInfoType = {
+      base_exp: resObj.base_experience,
+      name: resObj.forms[0].name,
+      height: resObj.height,
+      weight: resObj.weight,
+      stats: resObj.stats,
+      types: resObj.types,
+    };
+
+    return pokemonInfo;
+  }
 }
 
-export { ShallowLocations, Location, PokemonEncounter, PokeAPI };
+export {
+  ShallowLocations,
+  Location,
+  PokemonEncounter,
+  PokeAPI,
+  pokemonInfoType,
+};
