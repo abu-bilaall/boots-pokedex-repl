7ffb823fdca73c3af551c2e139a2f2b7bbf2671c
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-04-07 14:37:30 +0100

    refactor: catch chance and import fetchPokemonInfo

--- src/command_catch.ts
+++ src/command_catch.ts
@@ -1,38 +1,9 @@
 import { State } from "./state.js";
-import { z } from "zod";
+import { pokemonInfoType } from "./pokeapi.js";
 
-const EndpointResponseSchema = z.object({
-  base_experience: z.number(),
-  forms: z.array(z.object({ name: z.string(), url: z.string() })),
-  height: z.number(),
-  weight: z.number(),
-  stats: z.array(
-    z.object({
-      base_stat: z.number(),
-      effort: z.number(),
-      stat: z.object({ name: z.string(), url: z.string() }),
-    }),
-  ),
-  types: z.array(
-    z.object({
-      slot: z.number(),
-      type: z.object({ name: z.string(), url: z.string() }),
-    }),
-  ),
-});
+export async function catchCmd(state: State, ...args: string[]) {
+  const pokemonName = args.length > 0 ? args[0] : undefined;
 
-type EndpointResponse = z.infer<typeof EndpointResponseSchema>;
-
-export type pokemonInfoType = {
-  base_exp: EndpointResponse["base_experience"];
-  name: EndpointResponse["forms"][number]["name"];
-  height: EndpointResponse["height"];
-  weight: EndpointResponse["weight"];
-  stats: EndpointResponse["stats"];
-  types: EndpointResponse["types"];
-};
-
-export async function catchCmd(state: State, pokemonName?: string) {
   if (pokemonName === undefined) {
     console.log("You must specify a pokemon name");
     return;
@@ -40,27 +11,16 @@ export async function catchCmd(state: State, pokemonName?: string) {
 
   console.log(`Throwing a Pokeball at ${pokemonName}...`);
 
-  const urlEndpoint = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
-  const res = await fetch(urlEndpoint);
+  const pokemonInfo: pokemonInfoType =
+    await state.pokeapi.fetchPokemonInfo(pokemonName);
 
-  if (!res.ok) {
-    console.log(`${pokemonName} not found!`);
-  }
+  const MIN_CATCH_RATE = 0.05;
+  const MAX_CATCH_RATE = 0.95;
 
-  const json = await res.json();
-  const resObj: EndpointResponse = EndpointResponseSchema.parse(json);
-
-  const pokemonInfo = {
-    base_exp: resObj.base_experience,
-    name: resObj.forms[0].name,
-    height: resObj.height,
-    weight: resObj.weight,
-    stats: resObj.stats,
-    types: resObj.types,
-  };
-
-  const maxExp = 300;
-  const catchRate = 1 - pokemonInfo.base_exp / maxExp;
+  const catchRate = Math.max(
+    MIN_CATCH_RATE,
+    Math.min(MAX_CATCH_RATE, 1 - pokemonInfo.base_exp / 600),
+  );
 
   if (Math.random() < catchRate) {
     console.log(`${pokemonName} was caught!`);
